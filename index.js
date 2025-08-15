b41faad08de619f39d574bd51a161292297a55af
KrishnanM63 <krishnan.devpython46112121@gmail.com>
2025-08-15 07:55:13 +0530

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+function taiko(){
+    document.getElementById("taiko").play()
+}
+function joke(){
+    document.getElementById("joke").play()
+}
+function whoosh(){
+   document.getElementById("whoosh").play() 
+}
+function punchy(){
+    document.getElementById("punchy").play()
+}
